d6250be94ab83ac5e8badef9c2f151cee52afbce
Анастасія Савич <anastasiasavych@MacBook-Pro-Anastasia.local>
2022-04-11 21:59:07 +0300

    Initial commit

---
+++ src/com/company/SecondTask.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class SecondTask {
+
+    public static void main(String[] args) {
+        for(int a = 2;a<5000;a=2*a-1) {
+            System.out.println(a);
+        }
+    }
+}
---
+++ src/com/company/ThirdyTask.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class ThirdyTask {
+
+    public static void main(String[] args) {
+        for(int i =1;i<=10;i++)
+            if (10%i == 0){
+                System.out.println(i);
+            }
+        }
+
+
+
+
+
+    }
+
